84b874e8cc608e736a11b2e80fcded5efcb67022
sagar26197@gmail.com <sagar26197@gmail.com>
2019-01-22 18:19:01 -0500

    v2

--- L1-Q4/Program.cs
+++ L1-Q4/Program.cs
@@ -6,7 +6,15 @@ namespace L1_Q4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(" Enter the value of n :");
+            int n, sum=0;
+            n = int.Parse(Console.ReadLine());
+            for ( int i=1; i <= n; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine("Sum of the first n numbers is = {0}", +sum);
+            Console.ReadLine();
         }
     }
 }
